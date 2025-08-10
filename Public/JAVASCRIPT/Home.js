ea5549cf0d98779609e4adf26c123896c524c951
IbrahimBousba <hdydanb97@gmail.com>
2025-08-10 14:04:45 +0200

    changes to html and css

--- Public/JAVASCRIPT/Home.js
+++ Public/JAVASCRIPT/Home.js
@@ -1,16 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const menuBtn = document.getElementById('menuBtn');
-    const headList = document.querySelector('.headList');
-    const closeBtn = document.getElementById('closeBtn');
+  const menuBtn = document.getElementById('menuBtn');
+  const navList = document.getElementById('navList');
+  const closeBtn = document.getElementById('closeBtn');
 
-    menuBtn.addEventListener('click', () => {
-        headList.classList.add('show');
-        menuBtn.style.display = 'none';
-    });
+  menuBtn.addEventListener('click', () => {
+    navList.classList.add('show');
+    menuBtn.style.display = 'none';
+  });
 
-    closeBtn.addEventListener('click', () => {
-        headList.classList.remove('show');
-        menuBtn.style.display = 'block';
-    });
-
+  closeBtn.addEventListener('click', () => {
+    navList.classList.remove('show');
+    menuBtn.style.display = 'block';
+  });
 });
